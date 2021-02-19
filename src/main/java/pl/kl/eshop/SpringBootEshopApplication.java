14e267091ae4684a8c3316430949758aef3afcd2
Krzysztof Lezner <krzys3ek@gmail.com>
2021-02-19 20:05:33 +0100

    Initial commit

---
+++ src/main/java/pl/kl/eshop/SpringBootEshopApplication.java
@@ -0,0 +1,13 @@
+package pl.kl.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kl/eshop/SpringBootEshopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kl.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
